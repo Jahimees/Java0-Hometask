88ebed577add294439e862025d8a87d78d1761f2
Jahimees <36009821+Jahimees@users.noreply.github.com>
2019-02-23 12:25:12 +0200

    Update HomeTask2_1.java

--- Task 2/HomeTask2_1.java
+++ Task 2/HomeTask2_1.java
@@ -19,7 +19,7 @@ public class HomeTask2_1 {
 		
 		for (int i=0; i<count; i++) {
 			Random rand = new Random();
-			if (rand.nextInt(2)==1) {
+			if (rand.nextBoolean()==true) {
 				heads++;
 			} else {
 				tails++;
